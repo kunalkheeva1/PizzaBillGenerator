f76ddcc91db858dc00b563307759f9ab8aacba54
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-06-23 22:43:49 -0700

    added the method which increases the total price after adding the exrtra cheeese price, also added extraCheesePrice, extraToppingPrice and extraBagPrice properties for the usage in the class

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,6 +1,9 @@
 public class Pizza {
     private int price;
     private boolean veg;
+    private int extraCheesePrice= 2;
+    private double extraToppingPrice = 1.5;
+    private double extraBagPrice = .20;
 
     public Pizza(boolean veg) {
         this.veg = veg;
@@ -12,7 +15,11 @@ public class Pizza {
         }
     }
 
-//    public void getPizzaPizza(){
-//        System.out.println(this.price);
-//    }
+    //add extra cheese method
+    public void addExtraCheese(){
+        System.out.println("Extra Cheese Added");
+        this.price +=extraCheesePrice;
+    }
+
+
 }
