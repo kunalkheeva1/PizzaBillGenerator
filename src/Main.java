91157e205533fafaa1a19598ab81539908e9f950
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-06-23 22:28:13 -0700

    confirmed the prices of the pizzas wrt its type by creating getPizzaMethod

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+    Pizza pizza = new Pizza(true);
+        pizza.getPizzaPrice();
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,18 @@
 public class Pizza {
+    private int price;
+    private boolean veg;
+
+    public Pizza(boolean veg) {
+        this.veg = veg;
+        if(this.veg) {
+            this.price = 300;
+        }
+        else {
+            this.price = 400;
+        }
+    }
+
+//    public void getPizzaPizza(){
+//        System.out.println(this.price);
+//    }
 }
