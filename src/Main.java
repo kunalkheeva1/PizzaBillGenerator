931ea22d42fea0130f2d257b2b6dad532dd45952
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2023-06-24 23:12:46 -0700

    methods added and also limited the access of two methods of adding extra cheese and adding extra topping

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        Pizza pizza = new Pizza(true);
+//        Pizza pizza = new Pizza(true);
+//
+//        pizza.addExtraCheese();
+//        pizza.addExtraTopping();
+//        pizza.addExtraTopping();
+//        pizza.addExtraTopping();
+//        pizza.addExtraTopping();
+//        pizza.toGo();
+//        pizza.getBill();
+        DeluxPizza dp = new DeluxPizza(true);
+        dp.getBill();
 
-        pizza.addExtraCheese();
-        pizza.addExtraTopping();
-        pizza.addExtraTopping();
-        pizza.addExtraTopping();
-        pizza.addExtraTopping();
-        pizza.toGo();
-        pizza.getBill();
 
     }
 }
